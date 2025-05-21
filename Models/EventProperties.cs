1545c9842f39e0561bb36ea2a516c247986d02aa
namdinh1 <nam.dinh@livepayments.com>
2025-05-21 15:06:18 +0700

    add addRawEvent api

---
+++ Models/EventProperties.cs
@@ -0,0 +1,13 @@
+namespace DataTransform.Models;
+
+public sealed class EventProperties
+{
+    public string? ProductArea { get; set; }
+    public string? ProductValueStream { get; set; }
+    public string? UiScreenState { get; set; }
+    public string? EventId { get; set; }
+    public string? FlowConversion { get; set; }
+    public string? SessionId { get; set; }
+    public string? TerminalId { get; set; }
+    public string? CreatedDate { get; set; }
+}
